474757ef0a9ae6c51cd80b4a4a54ef269b254a6e
Giovanni Bianchini <giovannibianchini6@gmail.com>
2024-09-11 10:24:42 +0200

    updating

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,45 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from "nuxt/config"
 export default defineNuxtConfig({
-    app: {
-        head: {
-            charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1',
-            title: 'Cardfolio',
-            meta: [
-                { name: 'description', content: 'My personal digital business card' }
-            ],
-            link: [
-                //Favicon
-                { rel: 'icon', type: 'image/x-icon', href: '/images/logo/favicon.png' },
-                //Google Font
-                { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-                { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-                { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap' },
-                //UiKit CSS
-                { rel: 'stylesheet', type: "text/css", href: 'https://cdn.jsdelivr.net/npm/uikit@3.15.19/dist/css/uikit.min.css'},
-                //Bootstrap CSS
-                { rel: 'stylesheet', type: "text/css", href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'},
-            ],
-            script: [
-                //UiKit JS
-                { src: 'https://cdn.jsdelivr.net/npm/uikit@3.15.16/dist/js/uikit.min.js', async: true, body: true, },
-            ],
-        },
-    },
+  app: {
+      head: {
+          charset: 'utf-8',
+          viewport: 'width=device-width, initial-scale=1',
+          title: 'Cardfolio',
+          meta: [
+              { name: 'description', content: 'My personal digital business card' }
+          ],
+          link: [
+              //Favicon
+              { rel: 'icon', type: 'image/x-icon', href: '/images/logo/favicon.png' },
+              //Google Font
+              { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+              { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+              { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap' },
+              //UiKit CSS
+              { rel: 'stylesheet', type: "text/css", href: 'https://cdn.jsdelivr.net/npm/uikit@3.15.19/dist/css/uikit.min.css'},
+              //Bootstrap CSS
+              { rel: 'stylesheet', type: "text/css", href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'},
+          ],
+          script: [
+              //UiKit JS
+              { src: 'https://cdn.jsdelivr.net/npm/uikit@3.15.16/dist/js/uikit.min.js', async: true, tagPosition: 'bodyClose' },
+          ],
+      },
+  },
 
-    //setting runtime variable
-    runtimeConfig: {
-        public: {
-           
-        }
-    },
+  //setting runtime variable
+  runtimeConfig: {
+      public: {
+         
+      }
+  },
 
-    //loading main project css
-    css: [
-        '@/assets/css/main.css'
-    ],
+  //loading main project css
+  css: [
+      '@/assets/css/main.css'
+  ],
 
-    ssr: true,
+  ssr: true,
+  compatibilityDate: '2024-09-11',
 })
